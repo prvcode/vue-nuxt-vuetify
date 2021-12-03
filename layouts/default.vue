a28c32d861ae88e39447310acff1d125257c0b1d
Praveen <prvcode@gmail.com>
2021-12-03 10:42:19 +0530

    VNV-01: Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-avatar class="mr-5">
+      <img
+        src="/blockchain-logo.png"
+        alt="John"
+      >
+    </v-avatar>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed">
+      <lable>Praveen | 2021</lable>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      title: 'Blockchain'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card elevation="4" tile shaped>
+        <v-card-title>Create blockchain</v-card-title>
+        <v-card-text>
+           <v-row align="center" class="mx-0">
+             <v-file-input
+              accept="json/*"
+              label="Upload JSON File"
+            ></v-file-input>
+           </v-row>
+           <v-row align="center" class="mx-0">
+            <v-textarea
+              name="input-7-1"
+              label="Add JSON Object"
+            ></v-textarea>
+           </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
